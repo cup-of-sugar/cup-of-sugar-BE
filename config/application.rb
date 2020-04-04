55e2fbe3a5780e8aee936fe26bd0e440b5ef3533
Carleigh Crockett <capleugh@gmail.com>
2020-04-04 16:46:51 -0600

    Modify config/application to allow graphql to function in api only app
    Co-Authored-By: null <LainMcGrath@users.noreply.github.com>

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ require "action_mailbox/engine"
 require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
-# require "sprockets/railtie"
+require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -33,5 +33,6 @@ module CupOfSugarBe
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.use Rack::MethodOverride
   end
 end
