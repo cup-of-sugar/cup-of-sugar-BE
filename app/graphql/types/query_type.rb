5cd4b2a365462c3796f99dc9d7fab6cb222deef8
Carleigh Crockett <capleugh@gmail.com>
2020-04-05 13:01:05 -0600

    Create get_all_items query in query_type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,10 +1,7 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
-
-    # TODO: remove me
-    field :items, [Types::ItemType], null: false,
+  
+    field :get_all_items, [Types::ItemType], null: false,
       description: "Returns a list of items"
 
     def get_all_items
