b37f58bd08ea4fd61f7e94f63f33169497087cd8
Lain McGratH <lain.mcgrath@gusto.com>
2020-04-05 14:56:35 -0600

    deleted and re-added closing quote - srs that was the fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,5 +105,5 @@ Rails.application.configure do
 
 
   # Heroku app configuration
-  config.allowed_cors_origins = ["https://fierce-tundra-54482.herokuapp.com”]
+  config.allowed_cors_origins = ["https://fierce-tundra-54482.herokuapp.com"]
 end
