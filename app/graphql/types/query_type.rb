5b04ae88ca9c556f643ecee8965dacacea29c61f
Lain McGratH <lain.mcgrath@gusto.com>
2020-04-05 20:20:51 -0600

    Add end points for get_all_categories and get_all_category_items

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,11 +1,27 @@
 module Types
   class QueryType < Types::BaseObject
-  
+
     field :get_all_items, [Types::ItemType], null: false,
       description: "Returns a list of items"
 
     def get_all_items
       Item.all
     end
+
+    field :get_all_categories, [Types::CategoryType], null: false,
+      description: "Returns all categories in the database"
+
+    def get_all_categories
+      Category.all
+    end
+
+    field :get_all_category_items, [Types::ItemType], null: false, description: "Returns all category items" do
+      argument :id, ID, required: true
+    end
+
+    def get_all_category_items(id:)
+      Category.find(id).items
+    end
+
   end
 end
