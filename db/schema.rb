2dec5275244ae8b8b0fc1fdac02df50e656c7474
Lain McGratH <lain.mcgrath@gusto.com>
2020-04-06 16:44:29 -0600

    add returnable boolean to items
    Co-Authored-By: Carleigh Crockett <Capleugh@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_05_044315) do
+ActiveRecord::Schema.define(version: 2020_04_06_200101) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2020_04_05_044315) do
     t.string "description"
     t.string "measurement"
     t.string "time_duration"
+    t.boolean "returnable", default: true
     t.index ["category_id"], name: "index_items_on_category_id"
   end
 
