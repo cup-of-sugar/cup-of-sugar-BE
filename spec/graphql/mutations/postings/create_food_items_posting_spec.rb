da2ec6cc60e24dfcf718aad84363d5e771840c81
Carleigh Crockett <capleugh@gmail.com>
2020-04-09 16:42:37 -0600

    Create creatPosting mutation
    Co-authored-by: Lain McGrath
    <53873820+LainMcGrath@users.noreply.github.com>
    
    Create test for creating a posting which has items in a category other than food
    
    Co-authored-by: Lain McGrath
    <53873820+LainMcGrath@users.noreply.github.com>
    
    Change quantity field from string to a float
    
    Co-authored-by: Lain McGrath
    <53873820+LainMcGrath@users.noreply.github.com>

--- spec/graphql/mutations/postings/create_food_items_posting_spec.rb
+++ spec/graphql/mutations/postings/create_food_items_posting_spec.rb
@@ -4,18 +4,19 @@ module Mutations
   module Postings
     RSpec.describe CreatePosting, type: :request do
       describe 'create a posting' do
-        it 'for item to borrow' do
+        it 'for item to borrow in food category' do
         @food = Category.create(name: 'Food')
+
         expect(Posting.count).to eq(0)
         post "/graphql", params: { query: query }
         expect(Posting.count).to eq(1)
 
         result = JSON.parse(response.body, symbolize_names: true)
         posting = result[:data][:posting]
-        
+
         expect(posting[:name]). to eq("Butter")
         expect(posting[:description]). to eq("It's butter.")
-        expect(posting[:quantity]). to eq("8.0")
+        expect(posting[:quantity]). to eq(8.0)
         expect(posting[:measurement]). to eq("oz")
         end
       end
@@ -28,11 +29,10 @@ module Mutations
                 categoryName: "#{@food.name}"
                 name: "Butter"
                 description: "It's butter."
-                quantity: 8.0
+                quantity: "8.0"
                 measurement: "oz"
               }
             ) {
-              # categoryName
               name
               description
               quantity
