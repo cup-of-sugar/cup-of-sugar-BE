9673c341a363db70017de2e6061159568b6fa3e5
Lain McGratH <lain.mcgrath@gusto.com>
2020-04-09 19:41:20 -0600

    Add users table and index on items table

---
+++ db/migrate/20200410011517_create_user.rb
@@ -0,0 +1,11 @@
+class CreateUser < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :password
+      t.integer :zip
+    end
+  end
+end
