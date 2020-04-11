3c076c04fcffb069e4238caea377fd74bf762872
Carleigh Crockett <capleugh@gmail.com>
2020-04-10 18:30:38 -0600

    Update resolve method and add arguments and fields in create_posting file
    Co-authored-by: Lain McGrath 
    <53873820+LainMcGrath@users.noreply.github.com>

--- app/graphql/mutations/postings/create_posting.rb
+++ app/graphql/mutations/postings/create_posting.rb
@@ -1,6 +1,7 @@
 module Mutations
   module Postings
     class CreatePosting < ::Mutations::BaseMutation
+      argument :posting_type, String, required: true
       argument :category_name, String, required: true
       argument :name, String, required: true
       argument :description, String, required: false
@@ -8,6 +9,7 @@ module Mutations
       argument :quantity, String, required: true
       argument :measurement, String, required: false
 
+      field :posting_type, String, null: false
       field :category_name, String, null: false
       field :name, String, null: false
       field :description, String, null: true
@@ -15,9 +17,11 @@ module Mutations
       field :quantity, Float, null: false
       field :measurement, String, null: true
 
+
       def resolve(params)
         category = Category.find_by(name: params[:category_name])
         user = User.first
+
 
         if category.name == 'Food'
           category.items.create(name: params[:name], description: params[:description], quantity: params[:quantity], measurement: params[:measurement], returnable: false, user_id: user.id)
@@ -25,7 +29,7 @@ module Mutations
           category.items.create(name: params[:name], description: params[:description], quantity: params[:quantity], time_duration: params[:time_duration], user_id: user.id)
         end
         item = Item.last
-        Posting.create(item_id: item.id)
+        Posting.create(item_id: item.id, posting_type: params[:posting_type])
         item
       end
     end
