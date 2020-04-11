4739059733c93b4b3f945dd1075da00682110089
Carleigh Crockett <capleugh@gmail.com>
2020-04-10 18:30:38 -0600

    Remove user association from items
    Co-authored-by: Lain McGrath 
    <53873820+LainMcGrath@users.noreply.github.com>

--- app/graphql/mutations/postings/create_posting.rb
+++ app/graphql/mutations/postings/create_posting.rb
@@ -3,10 +3,12 @@ module Mutations
     class CreatePosting < ::Mutations::BaseMutation
       argument :category_name, String, required: true
       argument :name, String, required: true
+      argument :title, String, requred: true
       argument :description, String, required: false
       argument :time_duration, String, required: false
       argument :quantity, String, required: true
       argument :measurement, String, required: false
+      argument :user_id, ID, required: true
 
       field :category_name, String, null: false
       field :name, String, null: false
@@ -16,16 +18,17 @@ module Mutations
       field :measurement, String, null: true
 
       def resolve(params)
+        require "pry"; binding.pry
         category = Category.find_by(name: params[:category_name])
         user = User.first
 
         if category.name == 'Food'
-          category.items.create(name: params[:name], description: params[:description], quantity: params[:quantity], measurement: params[:measurement], returnable: false, user_id: user.id)
+          category.items.create(name: params[:name], description: params[:description], quantity: params[:quantity], measurement: params[:measurement], returnable: false)
         else
-          category.items.create(name: params[:name], description: params[:description], quantity: params[:quantity], time_duration: params[:time_duration], user_id: user.id)
+          category.items.create(name: params[:name], description: params[:description], quantity: params[:quantity], time_duration: params[:time_duration])
         end
         item = Item.last
-        Posting.create(item_id: item.id)
+        Posting.create(item_id: item.id, title: params[:title], poster_id: params[:user_id])
         item
       end
     end
