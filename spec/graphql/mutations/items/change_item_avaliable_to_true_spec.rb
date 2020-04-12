d5ca674296ef0a82f4d986780a71c93f21635f2e
Carleigh Crockett <capleugh@gmail.com>
2020-04-11 20:40:51 -0600

    Pass user_id to query
    Co-authored-by: Lain McGrath 
    <53873820+LainMcGrath@users.noreply.github.com>

--- spec/graphql/mutations/items/change_item_avaliable_to_true_spec.rb
+++ spec/graphql/mutations/items/change_item_avaliable_to_true_spec.rb
@@ -6,8 +6,8 @@ module Mutations
       describe 'update item availability to false' do
         it 'can change to false with mutation request' do
           food = Category.create(name: 'Food', id: 15)
-          user = User.create(first_name: 'Carole', last_name: 'Baskin', email: 'carole@tigers.com', password: 'password', zip: 80206)
-          posting = Posting.create(posting_type: 1, title: "Lending spare items from my garage", poster_id: user.id)
+          @user = User.create(first_name: 'Carole', last_name: 'Baskin', email: 'carole@tigers.com', password: 'password', zip: 80206)
+          posting = Posting.create(posting_type: 1, title: "Lending spare items from my garage", poster_id: @user.id)
 
           @food = food.items.create(name: 'Butter', quantity: 8, measurement: "oz", available: false, posting_id: posting.id)
 
@@ -32,6 +32,7 @@ module Mutations
           item: updateItemAvailability(
             input: {
               id: "#{@food.id}"
+              userId: #{@user.id}
               available: true
               name: "#{@food.name}"
             }
