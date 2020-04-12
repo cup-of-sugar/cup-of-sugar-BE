8290ba7a2274e35ea5558dded46c6919998345c3
Carleigh Crockett <capleugh@gmail.com>
2020-04-11 20:40:51 -0600

    Refactor item update
    Co-authored-by: Lain McGrath 
    <53873820+LainMcGrath@users.noreply.github.com>

--- app/graphql/mutations/items/update_item.rb
+++ app/graphql/mutations/items/update_item.rb
@@ -13,26 +13,34 @@ module Mutations
 
       def resolve(params)
         item = Item.find(params[:id])
-
         if item.posting.posting_type == "lend"
-          if item.posting.responder_id.to_s == params[:user_id] && item.available == false
+          update_lend_posting(item, params[:user_id])
+        else
+          update_borrow_posting(item, params[:user_id])
+        end
+        item
+      end
+
+      private
+
+        def update_lend_posting(item, user_id)
+          if item.posting.responder_id.to_s == user_id && item.available == false
             # responder_id is equal to borrower
             item.update(available: !item.available)
           else
             item.update(available: !item.available)
-            item.posting.update(responder_id: params[:user_id])
-            # do we care about overwriting responder_ids each time because if we do, we can't save a history of how many people have borrowed this same item.
-          end
-        else
-          if item.posting.poster_id.to_s == params[:user_id] && item.available == false
-            item.update(available: !item.available)
-          else
-            item.posting.update(responder_id: params[:user_id])
-            # require "pry"; binding.pry
+            item.posting.update(responder_id: user_id)
+            # do we care about overwriting responder_ids each time because if we do, we can't save a history of how many people have borrowed this same item
           end
         end
-        item
-      end
+
+        def update_borrow_posting(item, user_id)
+          if item.posting.poster_id.to_s == user_id && item.available == false
+            item.update(available: !item.available)
+          else
+            item.posting.update(responder_id: user_id)
+          end
+        end
     end
   end
 end
