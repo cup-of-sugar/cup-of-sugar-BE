540ac176c3ff16db5b3b3ea5f65f3ef7eeca3724
Carleigh Crockett <capleugh@gmail.com>
2020-04-12 20:14:42 -0600

    Create queries to get all lender postings and postings a lender has responded to
    Co-authored-by: Lain McGrath 
    <53873820+LainMcGrath@users.noreply.github.com>

--- app/graphql/types/posting_type.rb
+++ app/graphql/types/posting_type.rb
@@ -2,6 +2,7 @@ module Types
   class PostingType < Types::BaseObject
     field :id, ID, null: false
     field :item, Types::ItemType, null: false
+    field :title, String, null: false
     field :posting_type, String, null: false
   end
 end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -29,7 +29,25 @@ module Types
 
     def get_all_items_by_name(item_name:)
       posting_ids = Posting.where(posting_type: 1).ids
-      items = Item.where(name: item_name).where(posting_id: posting_ids)
+      Item.where(name: item_name).where(posting_id: posting_ids)
+    end
+
+    field :get_all_user_lend_postings, [Types::ItemType], null: false, description: "Returns all lend postings associated with a user" do
+      argument :user_id, ID, required: true
+    end
+
+    def get_all_user_lend_postings(user_id:)
+      ids = Posting.where(poster_id: user_id).ids
+      Item.where(posting_id: ids)
+    end
+
+    field :get_lend_posting_where_user_responder, [Types::ItemType], null: false, description: "Returns all lend postings where lender is responder" do
+      argument :user_id, ID, required: true
+    end
+
+    def get_lend_posting_where_user_responder(user_id:)
+      ids = Posting.where(responder_id: user_id).ids
+      Item.where(posting_id: ids)
     end
   end
 end
