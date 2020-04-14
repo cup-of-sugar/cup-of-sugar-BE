97952fc88bda5f19b10fe7012b38f2d89c195025
Lain McGratH <lain.mcgrath@gusto.com>
2020-04-13 20:03:11 -0600

    Add create user and signin mutations

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,6 +2,8 @@ module Types
   class MutationType < Types::BaseObject
     field :update_item_availability, mutation: Mutations::Items::UpdateItem
     field :create_posting, mutation: Mutations::Postings::CreatePosting
+    field :create_user, mutation: Mutations::Users::CreateUser
     field :user_login, mutation: Mutations::Users::UserLogin
+    field :sign_in_user, mutation: Mutations::Users::SignInUser
   end
 end
