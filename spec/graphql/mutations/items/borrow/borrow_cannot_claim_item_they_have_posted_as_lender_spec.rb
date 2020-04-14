8c70bbf15d40666f60e937ce3beb9793b0e7b7b1
Carleigh Crockett <capleugh@gmail.com>
2020-04-14 12:55:40 -0600

    Add validations on update item to confirm that responder_id and poster_id do not match
    Co-authored-by: Lain McGrath <53873820+LainMcGrath@users.noreply.github.com>

---
+++ spec/graphql/mutations/items/borrow/borrow_cannot_claim_item_they_have_posted_as_lender_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+module Mutations
+  module Items
+    RSpec.describe UpdateItem, type: :request do
+      describe 'borrower cannot claim an item' do
+        it 'if item was posted by user as a lender' do
+          food = Category.create(name: 'Food', id: 15)
+          @poster = User.create(first_name: 'Carole', last_name: 'Baskin', email: 'carole@tigers.com', password: 'password', zip: 80206)
+
+          posting = Posting.create(posting_type: 0, title: "Lending spare items from my garage", poster_id: @poster.id)
+
+          @food = food.items.create(name: 'Butter', quantity: 8, measurement: "oz", available: true, posting_id: posting.id)
+
+          post "/graphql", params: { query: query }
+
+          result = JSON.parse(response.body, symbolize_names: true)
+
+          expect(result[:data][:item]).to be_nil
+        end
+      end
+
+      def query
+        <<~GQL
+        mutation {
+          item: updateItemAvailability(
+            input: {
+              id: "#{@food.id}"
+              userId: #{@poster.id}
+              available: false
+              name: "#{@food.name}"
+            }
+          )
+          {
+            id
+            available
+            name
+          }
+        }
+        GQL
+      end
+    end
+  end
+end
