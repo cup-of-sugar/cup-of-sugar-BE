4093a39d0267d37a328e453c4799dade2265de12
Lain McGratH <lain.mcgrath@gusto.com>
2020-04-14 11:32:19 -0600

    add send_message mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,7 +4,6 @@ module Types
     field :create_posting, mutation: Mutations::Postings::CreatePosting
     field :create_user, mutation: Mutations::Users::CreateUser
     field :user_login, mutation: Mutations::Users::UserLogin
-    field :sign_in_user, mutation: Mutations::Users::SignInUser
     field :send_message, mutation: Mutations::Messages::SendMessage
   end
 end
