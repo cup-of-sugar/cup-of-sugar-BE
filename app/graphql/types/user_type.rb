2c4a3f971fe17bad1c32ecf6917576382ae07cd5
Lain McGratH <lain.mcgrath@gusto.com>
2020-04-13 20:03:23 -0600

    add first and last name fields

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -3,5 +3,7 @@ module Types
     field :id, ID, null: false
     field :email, String, null: true
     field :password, String, null: true
+    field :first_name, String, null: true
+    field :last_name, String, null: true
   end
 end
