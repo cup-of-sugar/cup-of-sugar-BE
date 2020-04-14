2a7a28c63c895e5ea6b8e7bbfd3638289e8f062d
Lain McGratH <lain.mcgrath@gusto.com>
2020-04-14 11:24:13 -0600

    Add messages relationships

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_11_224759) do
+ActiveRecord::Schema.define(version: 2020_04_14_023846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,15 @@ ActiveRecord::Schema.define(version: 2020_04_11_224759) do
     t.bigint "posting_id", null: false
     t.index ["category_id"], name: "index_items_on_category_id"
     t.index ["posting_id"], name: "index_items_on_posting_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.integer "sender_id"
+    t.integer "recipient_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "postings", force: :cascade do |t|
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,5 +7,11 @@ RSpec.describe User, type: :model do
 
     it { should have_many :responder_users }
     it { should have_many(:posters).through(:responder_users) }
+
+    it { should have_many :sending_users }
+    it { should have_many(:recipients).through(:sending_users)}
+
+    it { should have_many :recipient_users }
+    it { should have_many(:senders).through(:recipient_users)}
   end
 end
