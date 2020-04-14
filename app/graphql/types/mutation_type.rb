a8b93df712bdc9dfef514749b88d9f40f57df917
Lain McGratH <lain.mcgrath@gusto.com>
2020-04-14 15:31:07 -0600

    remove create_user mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,7 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     field :update_item_availability, mutation: Mutations::Items::UpdateItem
     field :create_posting, mutation: Mutations::Postings::CreatePosting
-    field :create_user, mutation: Mutations::Users::CreateUser
     field :user_login, mutation: Mutations::Users::UserLogin
     field :send_message, mutation: Mutations::Messages::SendMessage
   end
