393fa639a7b235f1f7d41bc30e735c4dc35d3827
Lain McGratH <lain.mcgrath@gusto.com>
2020-04-13 20:03:50 -0600

    add name and email validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ApplicationRecord
   has_many :responder_users, foreign_key: :responder_id, class_name: 'Posting'
   has_many :posters, through: :responder_users
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+
   has_secure_password
 
   def self.verify_login(params)
