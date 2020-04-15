738f493d27b771686e6d967b845dfa1001281b76
Lain McGratH <lain.mcgrath@gusto.com>
2020-04-14 21:34:21 -0600

    Add dependent destroy for items

--- app/models/posting.rb
+++ app/models/posting.rb
@@ -4,6 +4,6 @@ class Posting < ApplicationRecord
   belongs_to :poster, class_name: 'User'
   belongs_to :responder, class_name: 'User', optional: true
 
-  has_many :items
+  has_many :items, dependent: :destroy
   enum posting_type: ['borrow', 'lend']
 end
