d6d46662368747a81affe29d50c34962c87e09ba
Lain McGratH <lain.mcgrath@gusto.com>
2020-04-14 21:33:26 -0600

    Add delete posting mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,5 +4,6 @@ module Types
     field :create_posting, mutation: Mutations::Postings::CreatePosting
     field :user_login, mutation: Mutations::Users::UserLogin
     field :send_message, mutation: Mutations::Messages::SendMessage
+    field :delete_posting, mutation: Mutations::Postings::DeletePostingg
   end
 end
