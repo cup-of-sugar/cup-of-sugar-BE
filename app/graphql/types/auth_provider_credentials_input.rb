282590aef67ee6cce134410a4cebcf1b64fa27c8
Lain McGratH <lain.mcgrath@gusto.com>
2020-04-15 14:59:41 -0600

    add class

---
+++ app/graphql/types/auth_provider_credentials_input.rb
@@ -0,0 +1,9 @@
+module Types
+  class AuthProviderCredentialsInput < BaseInputObject
+
+    graphql_name 'AUTH_PROVIDER_CREDENTIALS'
+
+    argument :email, String, required: true
+    argument :password, String, required: true
+  end
+end
